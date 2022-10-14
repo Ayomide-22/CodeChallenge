d8f16c42555814ce4fa2c3a1f4c35767b581b76c
Ayomide-22 <ayomideagboola777@gmail.com>
2022-10-14 10:47:43 +0100

    Code Challenge

---
+++ 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Al_Ameen_Code_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array length");
+            int length=int.Parse(Console.ReadLine());
+            int[] arr = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("Enter number at index "+ i);
+                arr[i]= int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Enter number to check in array");
+            int original = int.Parse(Console.ReadLine());
+            int x= original;
+            for(int j=0;j<length;j++)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    if(original == arr[i])
+                    {
+                        original = original*2;
+                    }
+                }
+            }
+            if(original==x)
+            {
+                Console.WriteLine($"{original} is not found in the array");
+            }
+            else
+            {
+                Console.WriteLine(original+" is the final number");
+            }
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number to check");
+            double num=double.Parse(Console.ReadLine());
+            double[] arr=new double[3];
+            double mid=0;
+            double x=Math.Pow(10,15);
+            if(num>=x||num<=0)
+            {
+                Console.WriteLine(num+" is out of bounds");
+            }
+            else if(num%3==0)
+            {
+                mid=num/3;
+                arr[0]=mid-1;
+                arr[1]=mid;
+                arr[2]=mid+1;
+                foreach (var item in arr)
+                {
+                    Console.Write(item+" ");
+                }
+                Console.Write("add up to "+num);
+            }
+            else
+            {
+                Console.WriteLine("There aren't any three consecutive numbers that add up to "+num);
+            }
+        }
+    }
+}
